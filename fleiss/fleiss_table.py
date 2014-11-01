e8fe9c0697035d82449583dd9a5f8641ed55d445
Seth-David Donald Dworman <sdworman@brandeis.edu>
2014-11-01 17:10:16 -0400

    Moved row construction of table into its own function (considering making a class to represent a table...).  Row function now can make rows for all extents, not just those matched exactly across all annotators

--- fleiss/fleiss_table.py
+++ fleiss/fleiss_table.py
@@ -13,17 +13,46 @@ module to take the table as input.
 
 """
 
+import os
+import re
 import numpy
 import xml.etree.ElementTree as ET
 
+xml_pattern = re.compile(r'[0-9]+_[a-z]+_[0-9]+_[a-z]+\-[a-z][0-9]+\-p[0-9]+\.xml', re.IGNORECASE)
+
 #global list of ISO-Space tags which have extents, i.e. not relations.
 ISO_CATEGORIES = ['PLACE', 'PATH', 'SPATIAL_ENTITY', 'NONMOTION_EVENT',
-                  'MOTION', 'SPATIAL_SIGNAL', 'MOTION_SIGNAL', 'MEASURE']
+                  'MOTION', 'SPATIAL_SIGNAL', 'MOTION_SIGNAL', 'MEASURE', 'NONE']
 
 #example files, change these according to location on computer
 ISO_FILE = "/users/sethmachine/desktop/iso-space2/sprl_to_iso-space/CP/45_N_22_E.xml"
 f1 = '/users/sethmachine/desktop/WhereToLosAngeles_HOLLYWOOD-MS-p1.xml'
 f2 = '/users/sethmachine/desktop/WhereToLosAngeles_HOLLYWOOD-AB-p1.xml'
+
+#path to folder containing all xmls to be adjudicated
+ADJUDICATED_PATH = '/users/sethmachine/desktop/Adjudication'
+TEST_PATH = os.path.join(ADJUDICATED_PATH, '47_N_27_E')
+
+def getXmls(path):
+    """Finds all xml files in a flat directory.
+
+    Collects all xml files which match the xml_pattern.
+    The pattern excludes files with suffixes like "extentsLocked."
+
+    Args:
+        path: A string absolute path for the directory.
+
+    Returns:
+        A list of all xml files matching xml_pattern.
+    """
+    files = []
+    for f in os.listdir(path):
+        fpath = os.path.join(path, f)
+        if xml_pattern.match(f):
+            files.append(fpath)
+    return files
+
+test = getXmls(TEST_PATH)
 
 #relevant value names
 TEXT = 'TEXT'
@@ -49,25 +78,8 @@ def tag_matches(tag1, tag2):
             return True
     return False
 
-def get_table(xmls, categories = ISO_CATEGORIES):
-    """Builds the table for computing Fleiss' Kappa.
-
-    Constructs a numpy.zeros array of dimensions m * n, representing agreement amongst annotators.
-    m is the number of extents over all annotators and n is the number of categories/labels.
-    The extents are only considered if, for all xmls, that exact extent (based on ``tag_equals``)
-    exists in the other xmls.  Otherwise the extent is ignored.
-    Currently this does not consider non-consuming tags and ignores them.
-
-    Args:
-        xmls: A list of file paths, where each element is an annotated xml.
-            The xmls should all be mark up for the same text.
-        categories: A list of categories/tag types that are possible labels for
-            any given extent.  By default it includes all possible ISO-Space extents.
-
-    Returns:
-        An m * n numpy.zeros array, where each entry ij represents the number of
-        annotators who assigned label n_j to extent m_i.  The total across all rows
-        sums to the number of xmls/annotators.
+def get_rows(xmls, categories=ISO_CATEGORIES, use_unmatched=True):
+    """Builds the rows for each table entry
     """
     if not xmls:
         raise ValueError, "xmls input must contain at least one element"
@@ -98,19 +110,55 @@ def get_table(xmls, categories = ISO_CATEGORIES):
             matched_tags.append(matched)
         else:
             unmatched_tags.append(tag)
-        #don't uncomment this line below; it would break the function
-        #tag_dict[FIRST].remove(tag)
-    #build the numpy array
-    fleiss_table = numpy.zeros((len(matched_tags), len(ISO_CATEGORIES)), dtype=int)
+    if use_unmatched:
+        for tag in unmatched_tags:
+            matched = [tag]
+            for i in xrange(FIRST + 1, numXmls):
+                matched.append(ET.Element('NONE'))
+            matched_tags.append(matched)
+        for i in xrange(FIRST + 1, numXmls):
+            for tag in tag_dict[i]:
+                matched = []
+                for j in xrange(FIRST, numXmls):
+                    if j == i:
+                        matched.append(tag)
+                    else:
+                        matched.append(ET.Element('NONE'))
+                matched_tags.append(matched)
+        return matched_tags
+    return matched_tags
+
+def get_table(xmls, categories=ISO_CATEGORIES, use_unmatched=True):
+    """Builds the table for computing Fleiss' Kappa.
+
+    Constructs a numpy.zeros array of dimensions m * n, representing agreement amongst annotators.
+    m is the number of extents over all annotators and n is the number of categories/labels.
+    The extents are only considered if, for all xmls, that exact extent (based on ``tag_equals``)
+    exists in the other xmls.  Otherwise the extent is ignored.
+    Currently this does not consider non-consuming tags and ignores them.
+
+    Args:
+        xmls: A list of file paths, where each element is an annotated xml.
+            The xmls should all be mark up for the same text.
+        categories: A list of categories/tag types that are possible labels for
+            any given extent.  By default it includes all possible ISO-Space extents.
+
+    Returns:
+        An m * n numpy.zeros array, where each entry ij represents the number of
+        annotators who assigned label n_j to extent m_i.  The total across all rows
+        sums to the number of xmls/annotators.
+    """
+    matched_tags = get_rows(xmls, categories, use_unmatched)
+    fleiss_table = numpy.zeros((len(matched_tags), len(categories)), dtype=int)
     for row, matched in enumerate(matched_tags):
         for tag in matched:
-            column = ISO_CATEGORIES.index(tag.tag)
+            column = categories.index(tag.tag)
             fleiss_table[row][column] += 1
     return fleiss_table
     
 #uncomment for a quick test run
 #f = get_table([f1, f2])        
-        
+r = get_rows(test)       
         
 
 
