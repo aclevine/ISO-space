fb182fbc4007be835c6166da91b30455671c28d5
Seth-David Donald Dworman <sdworman@brandeis.edu>
2014-12-27 01:23:40 -0500

    utility functions for qslink calculations

---
+++ feature_engineering/corpus/util.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+"""Utility functions.
+
+.. moduleauthor:: Seth-David Donald Dworman <sdworman@brandeis.edu>
+
+"""
+from __future__ import division
+from collections import defaultdict
+from itertools import chain, combinations
+import os, re
+
+def getXmls(dirpath, recursive=True):
+    files = []
+    for f in os.listdir(dirpath):
+        fpath = os.path.join(dirpath, f)
+        if os.path.isfile(fpath):
+            if f.endswith('.xml'):
+                files.append(fpath)
+        if recursive:
+            if os.path.isdir(fpath):
+                files += getXmls(fpath, recursive)
+    return files
+
+def all_subsets(ss):
+  return chain(*map(lambda x: combinations(ss, x), range(0, len(ss)+1)))
+
+def check(attribs, subset, link):
+    for attrib in attribs:
+        if attrib in subset:
+            if link.attrib[attrib]:
+                continue
+        elif attrib not in subset:
+            if link.attrib[attrib]:
+                return False
+    return True
+
+def subset_str(subset):
+    return '_'.join([x for x in subset])
+
+id_pattern = re.compile(r'[0-9]+')
+
+def link_stats(linkdict):
+    z = {}
+    types = {}
+    for link_type in ['QSLINK', 'OLINK', 'MOVELINK']:
+        qs = linkdict[link_type]
+        attribs = ['toText', 'fromText', 'trigger']
+        d = defaultdict(list)
+        thistype = defaultdict(int)
+        for link in qs:
+            for subset in all_subsets(attribs):
+                if check(attribs, subset, link):
+                    d[subset_str(subset)].append(link)
+                    thistype[id_pattern.sub('', link.attrib['toID']) + '_to'] += 1
+                    thistype[id_pattern.sub('', link.attrib['fromID']) + '_from'] += 1
+                    thistype[id_pattern.sub('', link.attrib['trigger'])] += 1
+                    break
+        qs_count = len(qs)
+        print "============ " + link_type + " ============"
+        print "Total links of link type " + link_type + " : " + str(qs_count)
+        for key in d.keys():
+            count = len(d[key])
+            percent = count * 1.0 / qs_count
+            print "Total number of links of type <" + key + "> : " + str(count)
+            print "Percent of links of type <" + key + "> : " + str(percent)
+            print '\n'
+        z[link_type] = d
+        types[link_type] = thistype
+    return (types, z)
