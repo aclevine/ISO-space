694d9e1a71b9aee4eebae8b81dbb1dcaffd627b9
Zachary Yocum <zachary.yocum@me.com>
2015-01-26 19:49:12 -0500

    Updated to final results plots/ographs/CSV table

--- baseline_classifier/util/Corpora/stats.py
+++ baseline_classifier/util/Corpora/stats.py
@@ -2,44 +2,62 @@
 __author__ = "Zachary Yocum"
 __email__ = "zyocum@brandeis.edu"
 ################################################################################
+from collections import Counter
 from itertools import chain
+from operator import itemgetter
 from tokenizer import *
 from corpus import *
 
 # Root directory containing the corpus data files
 home = os.environ['HOME']
-space_eval_path = os.path.join(home, 'Dropbox/ISO-Space/SemEval/Training')
-# Corpus instance
-SpaceEval = Corpus(space_eval_path)
-# Extract the texts from each document and tokenize them
-texts = map(Document.text, SpaceEval.documents())
-tokenizers = (Tokenizer(text) for text in texts)
-tokenized_texts = map(Tokenizer.tokenize_text, tokenizers)
-# Generate sentence tokens
-sentences = chain(
-    *list(tokenized_text.sentences for tokenized_text in tokenized_texts)
+space_eval = os.path.join(home, 'Dropbox/ISO-Space/SemEval/')
+# Corpus instances
+train = Corpus(os.path.join(space_eval, 'Tokenized'))
+test = Corpus(os.path.join(space_eval, 'Test', 'Test'))
+anc = Corpus(os.path.join(space_eval, 'ANC'))
+dcp = Corpus(os.path.join(space_eval, 'DCP'))
+rfc = Corpus(os.path.join(space_eval, 'RFC'))
+corpora = {
+    'Train' : train,
+    'Test' : test,
+    'ANC' : anc,
+    'DCP' : dcp,
+    'RFC' : rfc
+}
+
+tag_types = (
+    'MOTION',
+    'MOTION_SIGNAL',
+    'NONMOTION_EVENT',
+    'PATH',
+    'PLACE',
+    'SPATIAL_ENTITY',
+    'SPATIAL_SIGNAL',
+    'MOVELINK',
+    'OLINK',
+    'QSLINK'
 )
-# Generate word tokens
-words = (sentence.tokens for sentence in sentences)
 
 def main():
-    # Accumulators for word and sentence token counts
-    word_count = 0
-    sentence_count = 0
-    # Iterate over all sentences
-    for sentence in sentences:
-        # Increment the counts
-        word_count += len(sentence.tokens)
-        sentence_count += 1
-    # Print the total counts
-    print '\n'.join([
-        'Total # of word tokens : {0}',
-        'Total # of sentence tokens : {1}'
-    ]).format(word_count, sentence_count)
-    # Print each text as a new-line separated list of tokens and sentence boundary markers
-    for document, tokenized_text in zip(SpaceEval.documents(), tokenized_texts):
-        out_file = document.name.split(os.sep)[-1].encode('utf-8')
-        tokenized_text.print_as_xmlstring()
+    for label, corpus in sorted(corpora.items()):
+        document_count = 0
+        sentence_count = 0
+        token_counter = Counter()
+        tag_counter = Counter()
+        for document in corpus.documents():
+            document_count += 1
+            sentence_count += len(document.tokenizer.sentences)
+            token_counter.update(token for _, _, token in document.tokenizer.lexes)
+            tag_counter.update(
+                tag.name for tag in document.tags(ttypes=tag_types)
+            )
+        token_count = len(list(token_counter.elements()))
+        print label
+        print '\t#documents : {}'.format(document_count)
+        print '\t#sentences : {}'.format(sentence_count)
+        print '\t#tokens : {}'.format(token_count)
+        for tag_type, count in tag_counter.items():
+            print '\t#{} : {}'.format(tag_type, count)
 
 if __name__ == '__main__':
     main()
